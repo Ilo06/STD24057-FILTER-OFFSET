5f159631368b3eb631a7f20454200f3885f512e1
Ilo <hei.iloniaina@gmail.com>
2025-12-05 11:14:09 +0300

    feat: init_db sql file

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE product_management_db;
+
+--creation de l'utilisateur
+CREATE USER product_manager_user WITH PASSWORD '123456';
+
+-- Attribution des privilèges à l'utilisateur sur la base de données
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+
+-- passer sur la nouvelle base de données
+\c product_management_db;
+
+-- garantir les privilèges sur le schéma public
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO product_manager_user;
+
+
