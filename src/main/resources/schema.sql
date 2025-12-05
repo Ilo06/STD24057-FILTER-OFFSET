f10c980ae9534ef8208c34dc1e545e57f4bcefdd
Ilo <hei.iloniaina@gmail.com>
2025-12-05 11:35:27 +0300

    feat: database schema sql files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Product_category(
+    id int primary key;
+    name varchar;
+)
+CREATE TABLE Product(
+    id int primary key;
+    name varchar;
+    price float;
+    creation_datetime timestamp;
+    category_id int,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES Product_category(id)
+);
+
