93d05fee026202ed580dd8ce87d7d16511144162
Ilo <hei.iloniaina@gmail.com>
2025-12-06 12:26:41 +0300

    feat: DBConnection class done

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,10 +1,27 @@
 import lombok.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
 @AllArgsConstructor
 public class DBConnection {
+    public Connection getDBConnection(){
+        String url = "jdbc:postgresql://localhost:5432/product_management_db";
+        String user = "product_manager_user";
+        String password = "123456";
 
+        try (Connection connection = DriverManager.getConnection(url, user, password);){
+            System.out.println("Connected to the database");
+            return connection;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
