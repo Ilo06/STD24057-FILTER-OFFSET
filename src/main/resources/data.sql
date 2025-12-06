fef3c790bf3782288a1c4f8b1a2adc29c53a8dbe
Ilo <hei.iloniaina@gmail.com>
2025-12-06 12:12:38 +0300

    feat: add insert on data sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Product(id, name,price, product_id) VALUES
+(1, 'Laptop Dell XPS', 4500.0, 1, 2024-01-15 09:30:00),
+(2, 'Iphone 13', 5200.0, 2, 2024-02-01 14:10:00),
+(3, 'Casque Sony WH1000', 890.50, 3, 2024-02-10 16:45:00),
+(4, 'Clavier Logitech', 180.0, 4, 2024-03-05 11:20:00),
+(5, 'Laptop Dell XPS', 4500.0, 1, 2024-03-16 08:00:00);
+
+
+INSERT INTO Product_Category(id, name, product_id) VALUES
+(1, Informatique, 1),
+(2, Telephonie, 2),
+(3, Audio, 3),
+(4, Accessoire, 4),
+(5, Informatique, 5),
+(6, Bureau, 5),
+(7, Mobile, 2);
